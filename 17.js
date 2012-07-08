2eac2dba16018958eb6b9b0b5340947798fd9a2f
Arthur Jamain <arthur.jamain@gmail.com>
2012-07-08 15:17:47 +0200

    17better

--- 17.js
+++ 17.js
@@ -5,39 +5,40 @@ var m = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eigh
     r = 0,
     d = new Date();
 
+String.prototype.fulltrim=function(){return this.replace(/(?:(?:^|\n)\s+|\s+(?:$|\n))/g,'').replace(/\s+/g,' ');}
+String.prototype.trim=function(){return this.replace(/^\s\s*/, '').replace(/\s\s*$/, '');};
+
 for(var i = 1 ; i <= 1000 ; i ++) {
+  var string = '';
   if(i < 20) {
-    r += b[i].length;
-    console.log(b[i]);
+    string += b[i];
   }
   else {
     var is = i+'';
-    
 
     if(is.length > 3) {
       if(i % 1000 == 0) {
-        r += 'one'.length;
-        r += o[1].length;
-        console.log('one', o[1]);
+        string += 'one'+o[1];
+        r += string.length;
+        console.log(string);
         break;
       }
     }
 
 
     if(is.length > 2) {
-        var j = parseInt(is.substr(is.length - 3, 1))
-        r += b[j].length;
-        r += o[0].length;
-      console.log(b[j],o[0]);
+      var j = parseInt(is.substr(is.length - 3, 1));
+      string += b[j]+''+o[0];
     }
 
-
+    if(is.length > 2 && i%100 != 0) {
+      string += 'and';
+    }
 
     var dozens = is.substr(is.length - 2, 2);
     if(dozens > 10 && dozens%10 == 0) {
       var j = parseInt(dozens.substr(0, 1));
-      r += m[j].length;
-      console.log(m[j]);
+      string += m[j];
     }
     else {
       var pid = parseInt(dozens);
@@ -45,17 +46,17 @@ for(var i = 1 ; i <= 1000 ; i ++) {
         if(pid == 0) {
           pid = parseInt(dozens.substr(1, 1))
         }
-        r += b[parseInt(dozens)].length;
-        console.log(b[pid]);
+        string += b[pid];
       }
       else {
         var j = parseInt(dozens.substr(0, 1)),
             k = parseInt(dozens.substr(1, 1));
-        r += b[k].length;
-        r += m[j].length;
-        console.log(m[j], b[k]);
+        string += m[j] + '' + b[k];
       }
     }
   }
+  r += string.length;
+  console.log(string, string.length);
 }
-console.log(r);
+console.log('Result (false) : ' + r);
+console.log('Elapsed: '+(new Date() - d)+'ms');
