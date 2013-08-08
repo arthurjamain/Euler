5a1fe3b0b0b45dc945f6aacf641ac05573d2fff9
Arthur Jamain <arthur.jamain@gmail.com>
2013-08-08 16:18:19 +0200

    Finally solved 18 ...

--- 18.js
+++ 18.js
@@ -2,16 +2,6 @@ var fs = require('fs'),
     f = [];
 var nbNodes = 0;
 
-// sum
-function sum(n) {
-  var i = 0;
-  for(var k = 1; k <= n ; k ++) {
-    i+=k;
-    console.log(k);
-  }
-  return n;
-}
-
 // object representing the whole graph
 function graph (lines) {
   this.nodes = [];
@@ -25,8 +15,8 @@ function graph (lines) {
       if(k == 0) {
 
         var opt = {
-          n1: 1, 
-          n2: 2, 
+          n1: 1,
+          n2: 2,
           index: nbNodes,
           val: ns[0]
         };
@@ -39,8 +29,8 @@ function graph (lines) {
           var rn1 = nbNodes + ns.length;
           var rn2 = rn1 + 1;
           var opt = {
-            n1: rn1, 
-            n2: rn2, 
+            n1: rn1,
+            n2: rn2,
             index: nbNodes,
             val: ns[j]
           };
@@ -53,10 +43,10 @@ function graph (lines) {
       var ns = lines[k].split(' ');
       for(var j in ns) {
         var opt = {
-          n1: -1, 
-          n2: -1, 
+          n1: -1,
+          n2: -1,
           index: nbNodes,
-          val: ns[j]
+          val: parseInt(ns[j], 10)
         };
         this.nodes.push(new node(opt));
         nbNodes++;
@@ -72,15 +62,51 @@ var node = function(opt) {
   this.n1 = opt.n1;
   this.n2 = opt.n2;
   this.index = opt.index;
-  this.val = opt.val;
+  this.val = parseInt(opt.val, 10);
   return this;
 }
 
+function sumnaturals(n)
+{
+    var i;
+    var sum = 0;
+    for(i = 1;i <= n; i++){
+        sum = sum + i;}
+    return (sum);
+}
 
 var t = fs.readFileSync('18-resource', 'utf8');
 var l = t.split('\n');
-var graph = new graph(l);
+var g = new graph(l);
+var c = 0;
+// For each line starting second from bottom
+for (var i = 14 ; i > 0 ; i--) {
 
-console.log(graph);
+  var start = sumnaturals(i - 1);
+  var threshold = (start + i);
 
-var pyramid = [];
+  for (var j = start ; j < threshold ; j++) {
+    c++;
+    var n = g.nodes[j];
+    var n1 = g.nodes[n.n1];
+    var n2 = g.nodes[n.n2];
+
+    if (!n1.sum || !n2.sum) {
+      n1.sum = n1.val;
+      n2.sum = n2.val;
+    }
+
+    var biggest = n1.sum < n2.sum ? n2 : n1;
+
+    n.sum = biggest.sum + n.val;
+    n.descendant = biggest.index;
+
+  }
+
+}
+
+console.log(g.nodes[0]);
+console.log(c + ' iterations');
+
+
+
